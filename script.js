32ecd6d13d6aea2c6ee5ba868d0d3e03bbfff481
darren <darren__c@hotmail.com>
2021-01-20 12:58:31 +0900

    neighbour country

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
-
+// xmlHttpequest
+/*
 const getCountryData = function (country) {
   const request = new XMLHttpRequest();
 
@@ -40,3 +41,64 @@ const getCountryData = function (country) {
 };
 
 getCountryData('usa');
+getCountryData('portugal');
+getCountryData('canada');
+
+*/
+
+// callback hell
+//added a className as parameter and the in the template - jonus made a css class for the neighbor country
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}" >
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)} million</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>  
+  </div>
+</article>
+
+
+`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  // countriesContainer.innerHTML = html;
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+  request.send();
+  console.log(request.responseText);
+  request.addEventListener('load', function () {
+    // console.log(this.responseText);
+    // const data = JSON.parse(this.responseText)[0];
+    const [data] = JSON.parse(this.responseText);
+    //render country 1
+    renderCountry(data);
+
+    //get neighbor country (2)
+    const [neighbor] = data.borders;
+    //some countries don't have borders so return immediately
+    if (!neighbor) return;
+
+    //ajax call 2
+    const request2 = new XMLHttpRequest();
+    // in the url string we changed the ...v2/NAME/&{country}  to ... v2/ALPHA/${NEIGHBOUR}
+    request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbor}`);
+    request2.send();
+    request2.addEventListener('load', function () {
+      console.log(this.responseText);
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+getCountryAndNeighbour('russia');
